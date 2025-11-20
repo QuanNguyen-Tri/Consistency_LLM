6b7097f7011ac1d97ab2ca6da745f76b40f0d734
Quan <nguyentriquan99@gmail.com>
2025-11-20 19:52:30 +0400

    a

--- eval/gsm8k/acc.sh
+++ eval/gsm8k/acc.sh
@@ -1,9 +1,10 @@
-# model_path=cllm/consistency-llm-7b-math
-model_path=GAIR/Abel-7B-001
+model_path=cllm/consistency-llm-7b-math
+# model_path=GAIR/Abel-7B-001
 target_model_path=GAIR/Abel-7B-001
 max_new_tokens=512
 
 # test model is tested and we use the tokenizer of teacher model because the tokenizer of test model has something to fix
+
 # CUDA_VISIBLE_DEVICES=0 python eval/gsm8k/acc.py --model_dir ${model_path} --temperature 0.0 --top_p 1.0 --output_file_name 'cllm_generated_gsm8k.jsonl' \
 #     --dev_set "gsm8k" --prompt_type math-single --max_new_tokens_for_consistency 512 --max_tokens 1024 --use_consistency_decoding
 
